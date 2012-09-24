8a5473d05510afe2d2fb12c0790fd1f3a0a942d9
Dr Nic Williams <drnicwilliams@gmail.com>
2012-09-24 06:02:04 -0700

    an initial test

---
+++ test/redis_elasticsearch_test.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env roundup
+
+describe "logstash with redis (broker) and elasticsearch (storage)"
+
+set -e # exit immediately if a simple command exits with a non-zero status
+set -u # report the usage of uninitialized variables
+
+[ "$(whoami)" != 'root' ] && ( echo ERROR: run as root user; exit 1 )
+
+cd /vagrant/ # need to hardcode as roundup overrides $0
+release_path=$(pwd)
+scripts=${release_path}/scripts
+
+rm -rf /tmp/before_all_run_already
+
+before_all() {
+  echo "|"
+  echo "| Stopping any existing jobs"
+  echo "|"
+  sm bosh-solo stop
+
+  echo "|"
+  echo "| Deleting /var/vcap/store databases"
+  echo "|"
+  rm -rf /var/vcap/store
+
+  # update deployment with example properties
+  example=${release_path}/examples/default.yml
+  sm bosh-solo update ${example}
+
+  # wait DBs & webapp to start
+  sleep 15
+  
+  # show last 20 processes (for debugging if test fails)
+  ps ax | tail -n 20
+}
+
+# before() is only hook into roundup
+# TODO add before_all() to roundup
+before() {
+  if [ ! -f /tmp/before_all_run_already ]
+  then
+    before_all
+    touch /tmp/before_all_run_already
+  fi
+}
+
+it_runs_redis() {
+  expected='redis-server /var/vcap/jobs/broker_redis/config/redis.conf'
+  test $(ps ax | grep "${expected}" | grep -v 'grep' | wc -l) = 1
+}
+
